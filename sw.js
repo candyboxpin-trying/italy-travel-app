ace7375fb290a28214c8875fc9ca7e4399c77f75
candyboxpin-trying <candybox.pin@gmail.com>
2026-06-30 20:25:43 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
 const CACHE_NAME = 'italia-2026-v1';
-const ASSETS = [
-  '/italia-2026/',
-  '/italia-2026/index.html',
-  '/italia-2026/manifest.json',
-  '/italia-2026/icon-192.png',
-  '/italia-2026/icon-512.png'
+const URLS_TO_CACHE = [
+  '/italy-travel-app/',
+  '/italy-travel-app/index.html',
+  '/italy-travel-app/icon-192.png',
+  '/italy-travel-app/icon-512.png'
 ];
 
-// 安裝：預先快取所有檔案
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// 啟動：清除舊版快取
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -25,15 +22,10 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// 攔截請求：有網路就更新，沒網路就用快取
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match('/italy-travel-app/index.html'));
+    })
   );
 });
